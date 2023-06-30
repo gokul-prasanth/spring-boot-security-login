590b449734b3d9dfa90c1abb197f756692c64d8c
Gokul Prasanth A <63594495+gokul-prasanth@users.noreply.github.com>
2023-06-29 20:50:23 -0600

    Add files via upload

---
+++ src/main/java/com/jwt/auth/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.jwt.auth.repository;
+
+import com.jwt.auth.model.ERole;
+import com.jwt.auth.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
